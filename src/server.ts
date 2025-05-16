8fff2a0f2f9627358d0dc0fddd2474ded7da100f
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-16 08:00:44 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,47 @@
 /* eslint-disable no-console */
-import mongoose from 'mongoose';
-import config from './config';
-import app from './app';
+import mongoose from 'mongoose'
+import config from './config'
+import app from './app'
 
-import { Server } from 'http';
+import { Server } from 'http'
 
-let server: Server;
+let server: Server
 
 process.on('uncaughtException', err => {
-  console.error(err);
-  process.exit(1);
-});
+  console.error(err)
+  process.exit(1)
+})
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.info(`🧑‍💻 Database connected successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`🧑‍💻 Database connected successfully`)
     server = app.listen(config.port, () => {
-      console.info(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.error(`Database is not connected`);
+    console.error(`Database is not connected`)
   }
   process.on('unhandledRejection', err => {
     console.log(
       'unhandled Rejection is detected we are closing our server......'
-    );
+    )
     if (server) {
       server.close(() => {
-        console.error(err);
-        process.exit(1);
-      });
+        console.error(err)
+        process.exit(1)
+      })
     } else {
-      process.exit(1);
+      process.exit(1)
     }
-  });
+  })
 }
 
-bootstrap();
+bootstrap()
 
 process.on('SIGTERM', () => {
-  console.info('SIGTERM IS RECEIVED');
+  console.info('SIGTERM IS RECEIVED')
   if (server) {
-    server.close();
+    server.close()
   }
-});
+})

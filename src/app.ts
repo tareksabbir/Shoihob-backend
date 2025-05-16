a500ca82e0c8ba29439fa811d28252b105fc9dae
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-16 20:18:10 +0600

    updated the deploy ci/cd

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import routes from './app/routes';
-import httpStatus from 'http-status';
-const app: Application = express();
+import express, { Application, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import routes from './app/routes'
+import httpStatus from 'http-status'
+const app: Application = express()
 
 // const store_id = process.env.Store_ID
 // const store_passwd = process.env.Store_Password
-// const is_live = false 
+// const is_live = false
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://your-frontend-domain.com'],
+    credentials: true,
+  })
+)
+
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/v1', routes);
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/v1', routes)
 
 // global error handler
-app.use(globalErrorHandler);
+app.use(globalErrorHandler)
 // handle not found route
 app.use((req: Request, res: Response) => {
   res.status(httpStatus.NOT_FOUND).json({
@@ -29,13 +35,12 @@ app.use((req: Request, res: Response) => {
         message: 'Api Not Found',
       },
     ],
-  });
+  })
   // next();
-});
+})
 
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({ status: 'OK' })
+})
 
-
-
-
-export default app;
-
+export default app
